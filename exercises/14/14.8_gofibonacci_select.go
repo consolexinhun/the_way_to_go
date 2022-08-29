86bf9883256355651c9bdb82f0ee466011fb4fb4
consolexin <consolexin@gmail.com>
2022-08-29 20:18:55 +0800

    go fibonacci

--- exercises/14/14.8_gofibonacci_select.go
+++ exercises/14/14.8_gofibonacci_select.go
@@ -1,18 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
+	start := time.Now()
 	c := make(chan int, 1)
 	quit := make(chan int, 1)
+
 	go func() {
-		for i := 0; i < 15; i++ {
+		for i := 0; i < 25; i++ {
 			fmt.Println(<-c)
 		}
 		quit <- 0
 	}()
 
 	fibonacci(c, quit)
+	end := time.Now()
+	delta := end.Sub(start)
+	fmt.Printf("longCalculation took amount of time : %s\n", delta)
 }
 
 func fibonacci(c, quit chan int) {
