56de7fe3eccb413409fbe4feab5653072d91099f
consolexin <consolexin@gmail.com>
2022-08-30 10:33:00 +0800

    concurrent pi

---
+++ exercises/14/14.11_concurrent_pi.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+
+	fmt.Println(CalculatePi(5000))
+
+	end := time.Now()
+	delta := end.Sub(start)
+	fmt.Println(delta)
+}
+
+func CalculatePi(n int) float64 {
+	ch := make(chan float64)
+	for k := 0; k <= n; k++ {
+		go term(ch, float64(k))
+	}
+
+	f := 0.0
+
+	for k := 0; k <= n; k++ {
+		f += <-ch
+	}
+	return f
+}
+
+func term(ch chan float64, k float64) {
+	ch <- 4 * math.Pow(-1, k) / (2*k + 1)
+}
